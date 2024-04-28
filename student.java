8f9137202a47611ac236652b52eaf2f3ec731c56
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-04-28 12:29:53 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,9 @@
+public class student {
+    public static void main(String args[]){
+        int id = Integer.parseInt(args[0]);
+        int name = Integer.parseInt(args[1]);
+        student s1 = new student();
+        System.out.println(s1.name);
+        System.out.println(s1.id);
+    }
+}
