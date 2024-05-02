e0780f2cb4233d5462c04e66fac27f9fe8354fe7
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-05-02 18:30:14 +0530

    Add files via upload

---
+++ uncheckexp.java
@@ -0,0 +1,18 @@
+import java.io
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;*;
+public class uncheckexp {
+    public static void findFile() throws IOException {
+        File newFile = new File("test.txt");
+        FileInputStream stream = new FileInputStream(newFile);
+    }
+    public static void main(String[] args) {
+        try{
+            findFile();
+
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+}
