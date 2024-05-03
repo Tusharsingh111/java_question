25914addf735aba7ec1345ab68144eac6d769698
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-05-03 18:04:47 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial{
+    public static void main(String args[]){
+            int i ,ft=1;
+            int num = 5;
+            for(i=1; ft<=num ; i++){
+                ft = ft*i;
+                }
+                System.out.println(ft);
+    }
+    }
