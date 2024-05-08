e5351b5c2588e44fe8bdda8611b30aaada3a84b5
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-05-09 01:23:53 +0530

    Add files via upload

---
+++ fs.java
@@ -0,0 +1,37 @@
+/*Different type conversions using java */
+// public class fs{
+//     public static void main(String args[]){
+//         double a=0.2368464826428D;
+//         System.out.println(a);
+//     }
+// }
+
+// public class fs{
+//     public static void main(String args[]){
+//         int a=1;
+//         System.out.println(a);
+//     }
+// }
+// public class fs{
+//     public static void main(String args[]){
+//         long a=123456789L;
+//         System.out.println(a);
+//     }
+// }
+
+// public class fs{
+//     public static void main(String[] args){
+//         double a=10.6;
+//         int b = (int)a;
+
+//         System.out.println(b);
+//     }
+// }
+public class fs{
+    public static void main(String[] args){
+        float a= (float)6.666;
+        int b = (int)a;
+
+        System.out.println(b);
+    }
+}
