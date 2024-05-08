b3c64662bdad4c2a25df4322e9df62f9cc6cb82f
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-05-08 23:32:31 +0530

    Add files via upload

---
+++ exception5.java
@@ -0,0 +1,11 @@
+public class exception5 {
+    static int sum(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        //try{
+            //sum(10);
+            ABC obj = new ABC();
+            
+    }
+}
