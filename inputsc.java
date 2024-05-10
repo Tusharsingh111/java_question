15677f08576e32780d58e40789d34e06f76278d0
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-05-10 18:58:24 +0530

    Add files via upload

---
+++ inputsc.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class inputsc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("The value of x is: "+x);
+    }
+}
