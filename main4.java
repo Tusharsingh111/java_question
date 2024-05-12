5c982bbd5e022d1362baf3474689085559322e52
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-05-12 13:08:21 +0530

    Add files via upload

---
+++ main4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class main4 {
+    public static void main(String[] args) {
+        // try{
+            Scanner sc = new Scanner(System.in);
+        //     int a = sc.nextInt();
+        //     int b = sc.nextInt();int c = a/b;
+        //     System.out.println(c);
+        // }catch(ArithmeticException e){
+        //     System.out.println("please enter a non zero value");
+        // }
+        // System.out.println("Hola");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a/b;
+
+    }
+}
