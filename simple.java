7a9b9d50cd04c4b63155fe6ffc3b150f66284dcd
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-05-13 21:09:53 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,12 @@
+public class simple {
+    static int a;
+    public static void main(String args[]){
+        float f = (float)1.5;
+        String s="Guts and Glories is all about jaat Story";
+        double d=1.3546788888;
+        System.out.println(a);
+        System.out.println("hello java");
+        System.out.println(s);
+        System.out.println(f);
+    }
+}
