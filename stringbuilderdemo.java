28c40f66f45f062defcd6bcfb5f2ed4b93ed5c9a
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-05-13 21:10:56 +0530

    Add files via upload

---
+++ stringbuilderdemo.java
@@ -0,0 +1,7 @@
+public class stringbuilderdemo {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("New Zealand");
+        System.out.println("length="+sb.length());
+        System.out.println("capacity"+sb.capacity());
+    }
+}
