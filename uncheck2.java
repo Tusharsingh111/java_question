ad34c13db271d57e1c0495f88fa45fb6c8815eaa
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-05-15 16:39:12 +0530

    Add files via upload

---
+++ uncheck2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class uncheck2 {
+    public static void findFile() throws IOException{
+        throw new IOException("File not found");
+    }
+    public static void main(String[] args) {
+        try{
+            findFile();
+            System.out.println("Rest of code in try block");
+
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
